bfb6b8d731c62db5671a5a8c436d812571bda806
Sanjay Rautela <140192191+sanjayrautela@users.noreply.github.com>
2024-01-17 17:33:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+  function focusNext(currentInput) {
+            if (currentInput.value.length === 1) {
+                // Move to the next input field
+                const nextInput = currentInput.nextElementSibling;
+                if (nextInput) {
+                    nextInput.focus();
+                }
+            } else if (currentInput.value.length === 0) {
+                // Move to the previous input field on backspace
+                const prevInput = currentInput.previousElementSibling;
+                if (prevInput) {
+                    prevInput.focus();
+                }
+            }
+        }
